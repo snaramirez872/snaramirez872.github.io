10350e2171ec074ca6921941a33c9d157bbf5549
snaramirez872 <seanramirez818@gmail.com>
2023-11-29 22:48:57 -0500

    added back files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import NavBar from "./components/NavBar";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <section id="about">
+        <h1>About Me</h1>
+      </section>
+      <section id="projects">
+        <h1>Projects</h1>
+      </section>
+      <section id="contact">
+        <h1>Contact Me</h1>
+      </section>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { Link } from 'react-scroll';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import './styles/NavBar.css';
+
+const Navbar = () => {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setMobileMenuOpen(!mobileMenuOpen);
+    };
+
+    return (
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link to="about" smooth duration={500} className="navbar-logo">
+                    Sean Ramirez
+                </Link>
+
+                <div className="navbar-menu-icon" onClick={toggleMobileMenu}>
+                    {mobileMenuOpen ? <FaTimes /> : <FaBars />}
+                </div>
+
+                <ul className={`navbar-menu ${mobileMenuOpen ? 'active' : ''}`}>
+                    <li className="navbar-item">
+                        <Link to="about" smooth duration={500}>About</Link>
+                    </li>
+                    <li className="navbar-item">
+                        <Link to="project" smooth duration={500}>Projects</Link>
+                    </li>
+                    <li className="navbar-item">
+                        <Link to="contact" smooth duration={500}>Contact Me</Link>
+                    </li>
+                    <li className="navbar-item">
+                        <a href="../public/test.txt" target="_blank" rel="noopener noreferrer">View Resume</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
