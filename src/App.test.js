a314d585ef88c9eb980b0844ddf0fb55c8ee750b
snaramirez872 <seanramirez818@gmail.com>
2023-11-30 23:38:23 -0500

    forgot a line

--- src/App.test.js
+++ src/App.test.js
@@ -4,9 +4,6 @@ import App from './App';
 test('renders webpage', () => {
   render(<App />);
 
-  // Log the entire rendered component for debugging
-  console.log(screen.debug());
-
   // Check if certain elements are present
   expect(screen.getAllByText("Projects")).toBeInTheDocument();
   expect(screen.getAllByText("Contact Me")).toBeInTheDocument();
