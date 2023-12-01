60ebdba7568ef7a38595c96751e84a6c33b4b90b
snaramirez872 <seanramirez818@gmail.com>
2023-11-30 23:36:35 -0500

    tests 3

--- src/App.test.js
+++ src/App.test.js
@@ -3,15 +3,12 @@ import App from './App';
 
 test('renders webpage', () => {
   render(<App />);
-  //const linkElement = screen.getByText(/learn react/i);
-  //expect(linkElement).toBeInTheDocument();
 
-  const eleOne = screen.getByText("About Me")
-  expect(eleOne).toBeInTheDocument();
+  // Log the entire rendered component for debugging
+  console.log(screen.debug());
 
-  const eleTwo = screen.getByText("Projects")
-  expect(eleTwo).toBeInTheDocument();
-
-  const eleThree = screen.getByText("Contact Me")
-  expect(eleThree).toBeInTheDocument();
+  // Check if certain elements are present
+  expect(screen.getByText("About Me")).toBeInTheDocument();
+  expect(screen.getByText("Projects")).toBeInTheDocument();
+  expect(screen.getByText("Contact Me")).toBeInTheDocument();
 });
