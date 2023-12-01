13a3cbba1ca202c540d5efb80255c81778cfa92e
snaramirez872 <seanramirez818@gmail.com>
2023-11-30 20:00:21 -0500

    'About Me' Done

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -21,17 +21,17 @@ const AboutMe = () => {
                         <ul>
                             <li>RedwoodJS</li>
                         </ul>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <ul>
-                            <li>BootStrap</li>
-                            <li>TailWindCSS</li>
-                        </ul>
-                        <li>C/C++</li>
-                        <li>Python</li>
-                        <li>MongoDB</li>
-                        <li>WSL</li>
                     </ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <ul className="tech-sub-list">
+                        <li>BootStrap</li>
+                        <li>TailWindCSS</li>
+                    </ul>
+                    <li>C/C++</li>
+                    <li>Python</li>
+                    <li>MongoDB</li>
+                    <li>WSL</li>
                 </ul>
             </div>
         </section>
